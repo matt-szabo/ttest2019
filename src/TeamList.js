a71c5b651ed86dd238c433103a4457c4120027b8
matt-szabo <mattidj@gmail.com>
2019-08-29 00:51:36 -0400

    added static.json file for Heroku

--- src/TeamList.js
+++ src/TeamList.js
@@ -96,7 +96,7 @@ handleChange = (event) => {
             <TableBody style={{borderBottom: "none" }}>
                 {this.state.dataToDisplay.map((item,i) => (
                   <TableRow key={i}>
-                      <Link to={{pathname:`/team/${item.id}`}} style={{ textDecoration: 'none' }}>
+                      <Link to={{pathname:`/team/${item.id}`}} style={{ textDecoration: 'none', :hover:{background-color:'red'} }}>
                           <TableCell>{item.id}</TableCell>
                           <TableCell>{item.name}</TableCell>
                       </Link>
